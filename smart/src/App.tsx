45ade07682c1225020882b5a468b4dcae87e1932
Ilya Beda <ir4y.ix@gmail.com>
2023-11-15 12:59:37 +1100

    Display only patient names
    Leverage fhir types

--- smart/src/App.tsx
+++ smart/src/App.tsx
@@ -1,3 +1,4 @@
+import { Bundle, Patient } from "fhir/r4b";
 import Client from "fhirclient/lib/Client";
 import { useEffect, useState } from "react";
 
@@ -6,10 +7,19 @@ interface AppProps {
 }
 
 export function App({client}:AppProps) {
-    const [patients, setPatients] = useState<any>(null);
+    const [patients, setPatients] = useState<Bundle<Patient>|null>(null);
     useEffect(() => {
         client.request("Patient").then(setPatients);
     }, [])
 
-    return (<pre>{JSON.stringify(patients, undefined, 4)}</pre>);
+
+    if(patients){
+        return <ul>
+            {patients.entry?.map(p => {
+                return <li>{JSON.stringify(p.resource?.name, undefined, 4)}</li>
+            })}
+        </ul>
+    } else {
+        return <p>Loading...</p>
+    }
 }
