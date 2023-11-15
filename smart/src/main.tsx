5387796f5d01a83d07b438ef2638f85be18b139d
Ilya Beda <ir4y.ix@gmail.com>
2023-11-15 13:37:29 +1100

    FHIR server base url can be changed

--- smart/src/main.tsx
+++ smart/src/main.tsx
@@ -4,26 +4,38 @@ import {App} from './App'
 
 const root = createRoot(document.getElementById("root")!);
 
+const FHIRServerUrl = "http://localhost:8080";
+/* const FHIRServerUrl = "https://au-core.beda.software"; */
+
+interface LaunchData {
+    result: {
+        uri: string,
+        launch: string,
+    }
+}
+
 function launch(){
-    fetch('http://localhost:8080/rpc', {
+    fetch(`${FHIRServerUrl}/rpc`, {
         method: 'POST',
         headers: {
             "Content-Type": "application/json",
         },
         body: JSON.stringify({
             method: 'aidbox.smart/get-launch-uri',
-            params: { iss: encodeURIComponent(`http://localhost:8080/fhir`), client: 'smart-app', user: 'admin'}
+            params: { iss: encodeURIComponent(`${FHIRServerUrl}/fhir`), client: 'smart-app', user: 'admin'}
             })
     }).then((response) => {
         return response.json()
-    }).then(data => {
+    }).then((data:LaunchData) => {
+        localStorage.setItem('launch', data.result.launch)
         window.location.href = data.result.uri
     })
 }
 
 SMART.init({
     clientId: "smart-app",
-    scope: "system/patient.r"
+    scope: "system/patient.r",
+    launch: localStorage.getItem('launch')!,
 })
      .then((client) => {
          root.render(<App client={client} />);
